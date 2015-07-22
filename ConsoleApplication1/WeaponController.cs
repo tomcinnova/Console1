354b760d2a6adad6f33911339caa5ee8f8329237
Tom C <tomc@innova.com>
2015-07-22 10:16:45 -0700

    initial console1 commit

---
+++ ConsoleApplication1/WeaponController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class WeaponController
+    {
+        private Weapon primaryWeapon { get; set; }
+        private Weapon sideArm { get; set; }
+        
+        public WeaponController(Weapon first, Weapon second)
+        {
+            primaryWeapon = first;
+            sideArm = second;
+        }
+    }
+}
